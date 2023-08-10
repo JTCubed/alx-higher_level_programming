14cdf18f9a78fb42a2d094b2f74fbabcf3ef03ff
JTCubed <zeusio60@gmail.com>
2023-08-10 04:08:37 +0000

    hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(9):
-        print("{}{}, ".format(i, j))
-        if i == 9 and j == 9:
-            print()
+for i in range(10):
+    for j in range(10):
+        if i != 9 and j != 9:
+            print("{}{},".format(i, j), end = " ")
+        elif i == 9 and j == 9:
+            print("{}{}".format(i, j))
