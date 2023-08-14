f1acb0b7074f10fede9b85bcf7c79975a3084b4c
JTCubed <zeusio60@gmail.com>
2023-08-13 20:51:59 -0700

    Hot fix to file 11

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     list_s = []
-    count = 0
     for i in my_list:
         if i % 2 == 0:
             list_s.append(True)
         else:
             list_s.append(False)
-        count += 1
     return(list_s)
