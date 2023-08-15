551be082fc5dca9d5253987efb6370d2ae112d45
JTCubed <zeusio60@gmail.com>
2023-08-15 16:59:10 -0700

    file 11 fixed

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,6 +4,7 @@ def delete_at(my_list=[], idx=0):
     if my_list is None:
         my_list1 = []
     if idx < 0 or idx > len(my_list) - 1:
-        return (my_list1)
+        return (my_list)
     else:
-        return (my_list1[:idx] + my_list1[idx+1:])
+        my_list[:] = (my_list[:idx] + my_list[idx+1:])
+        return (my_list)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,8 @@ def max_integer(my_list=[]):
     a = my_list[0]
     if my_list is None:
         return (None)
+    if my_list == []:
+        return (a)
     for i in my_list:
         if i >= a:
             a = i
