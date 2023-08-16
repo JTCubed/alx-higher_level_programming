8a47097ee8aa2196b49f545a291404557f864a39
JTCubed <zeusio60@gmail.com>
2023-08-15 20:39:46 -0700

    file 13 added

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,7 +2,8 @@
 
 int is_palindrome(listint_t **head)
 {
-    int i;
+    int stack[1024];
+    int i = 0;
     listint_t *current, *rev;
 
     current = *head;
@@ -12,7 +13,19 @@ int is_palindrome(listint_t **head)
 
     while(rev && rev->next)
     {
+	stack[i++] = current->n;
 	rev = rev->next->next;
 	current = current->next;
     }
+
+    if (rev)
+	current = current->next;
+
+    while (current)
+    {
+	if (current->n != stack[--i])
+	    return (0);
+	current = current->next;
+    }
+    return (1);
 }
