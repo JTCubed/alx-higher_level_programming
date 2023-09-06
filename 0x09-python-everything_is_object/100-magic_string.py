f37fee1fc4f1b8db0bb1cbf7cb0cf9b375a7960e
JTCubed <zeusio60@gmail.com>
2023-09-06 07:22:18 -0700

    Hot fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""returns a string “BestSchool” n times the number of the iteration"""
+
+
 def magic_string():
+    """returns a string “BestSchool” n times the number of the iteration"""
     magic_string.counter = getattr(magic_string, "counter", -1) + 1
     return ", ".join(["BestSchool"] * (magic_string.counter + 1))
