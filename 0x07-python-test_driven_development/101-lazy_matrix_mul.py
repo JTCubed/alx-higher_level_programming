bc1d08b50174c8b01d02d5c99ff19d1a9f93e9fb
JTCubed <zeusio60@gmail.com>
2023-10-02 13:21:25 -0700

    files added

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Multiplies matrices using numpy"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies 2 matrices using NumPy."""
+
+    return np.dot(m_a, m_b)
