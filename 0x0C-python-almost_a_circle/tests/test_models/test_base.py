2647cb42a7be1f2ee5fbbbcfa8090bc9b866ce56
JTCubed <zeusio60@gmail.com>
2023-11-13 11:46:03 -0800

    files 1 and 2 added

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,18 @@
+import unittest
+
+from models.base import Base
+
+class test_Base(unittest.TestCase):
+
+    def test_init(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
