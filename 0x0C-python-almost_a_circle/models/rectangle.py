f469e0286489edaef55e59287f191190acd97080
JTCubed <zeusio60@gmail.com>
2023-11-13 13:36:06 -0800

    hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-
 from models.base import Base
 
+"""
+Rectangle claass
+defines a rectangle by methods to print, update, validate and callculate
+the area
+"""
+
+
 class Rectangle(Base):
+    """
+    Rectangle claass
+    defines a rectangle by methods to print, update, validate and callculate
+    the area
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         Base.__init__(self, id)
