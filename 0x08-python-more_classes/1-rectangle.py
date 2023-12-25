a1d2ee636e1997175a4ea8f58fa1cb0866fb6a14
JTCubed <zeusio60@gmail.com>
2023-12-25 11:08:06 -0800

    Hot fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
     """an empty class"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -30,6 +30,10 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+if __name__ == "__main__":
+    x = Rectangle(3, -1)
+    print(x.__dict__)
