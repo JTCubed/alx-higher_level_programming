7a3f27f674eca790e2300dd4563fda02cd6da62b
JT <107570310+JTCubed@users.noreply.github.com>
2024-02-26 22:36:13 +0200

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/2-remove_database.sql
+++
@@ -1,3 +0,0 @@
--- deletes the database hbtn_0c_0
--- only if it exists
-DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++
@@ -1,4 +0,0 @@
--- inserts a new row in the table first_table
--- id = 89
--- name = Best School
-INSERT INTO first_table VALUES(89, 'Best School');
