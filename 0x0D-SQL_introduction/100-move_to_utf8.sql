f7f04df5a7d75a0259d96ad1cd40aec72df16c20
JTCubed <zeusio60@gmail.com>
2024-04-06 11:33:12 -0700

    hot fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,10 @@
 -- Alter databse to encoding utf8 COLLATE utf8mb4_unicode_ci
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
