37667aa35de39422f45b052b5d7b2db56d55a228
JTCubed <zeusio60@gmail.com>
2024-04-25 16:20:07 -0700

    Hot fix to file 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response_size=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-echo $response_size
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
+response_size=$(curl -s -o /dev/null -w "%{size_download}" "$1"); echo $response_size
