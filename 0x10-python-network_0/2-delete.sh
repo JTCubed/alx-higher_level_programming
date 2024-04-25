526e5ad3c39063c8631381ab082df2bcce1c295d
JTCubed <zeusio60@gmail.com>
2024-04-25 16:23:33 -0700

    Hot fix for files 1 - 4

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response_body=$(curl -s -X DELETE "$1")
-echo "$response_body"
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+response_body=$(curl -s -X DELETE "$1"); echo "$response_body"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-allowed_methods=$(curl -s -X OPTIONS -I "$1" | grep "Allow:" | awk '{print substr($0, index($0,$2))}')
-echo $allowed_methods
+# takes in a URL and displays all HTTP methods the server will accept.
+allowed_methods=$(curl -s -X OPTIONS -I "$1" | grep "Allow:" | awk '{print substr($0, index($0,$2))}'); echo $allowed_methods
