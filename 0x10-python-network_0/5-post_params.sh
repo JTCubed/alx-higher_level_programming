09df7b56927621622d73172b6be6a5b090a57fcd
JTCubed <zeusio60@gmail.com>
2024-04-25 17:06:01 -0700

    Hot fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-email="test@gmail.com"; subject="I will always be here for PLD";response_body=$(curl -s -X POST -d "email=$email&subject=$subject" "$1"); echo "$response_body"
+email="test@gmail.com"; subject="I will always be here for PLD";response_body=$(curl -s -X POST -d "email=$email&subject=$subject" "$1") && echo "$response_body"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Find peak in a list"""
+
 def find_peak(list_of_integers):
     """Find peak in a list"""
     if not list_of_integers:
