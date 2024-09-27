d27f4d3d1c2b9800f8228a9b26290569a167b46a
JTCubed <zeusio60@gmail.com>
2024-09-27 06:12:23 -0700

    file 4 modified

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -75,3 +75,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle to recreate it."""
+        return f"Rectangle({self.__width}, {self.__height})"
