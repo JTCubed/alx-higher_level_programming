f08ab83ad4c649eac8115e90aa11fc71ff511ec6
JTCubed <zeusio60@gmail.com>
2025-04-29 18:38:59 -0700

    hot fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,33 +2,29 @@
 
 const { argv } = require('node:process');
 
-let arr = []
+const arr = [];
 if (!argv[3]) {
-    console.log(0);
+  console.log(0);
 } else {
-
-    let num = 2
-    for (let i = 2; i < argv.length; i++) {
-        arr.push(parseInt(argv[i]));
-    }
+  for (let i = 2; i < argv.length; i++) {
+    arr.push(parseInt(argv[i]));
+  }
 //    console.log(arr.max);
 }
 
-
 function secondH () {
+  let max = -Infinity;
+  let sec = -Infinity;
 
-    let max = -Infinity, sec=-Infinity;
-
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] > max) {
-            sec1 = max;
-            max = arr[i];
-        } else if (arr[i] > sec && arr[i] !== max) {
-            sec = arr[i]
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      //      sec1 = max;
+      max = arr[i];
+    } else if (arr[i] > sec && arr[i] !== max) {
+      sec = arr[i];
     }
-    return (sec === -Infinity) ? 0 : sec;
+  }
+  return (sec === -Infinity) ? 0 : sec;
 }
 
 console.log(secondH(arr));
