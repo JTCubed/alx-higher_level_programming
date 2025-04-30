033e6cb370bf0ce29a022ab4537d3e097328441c
JTCubed <zeusio60@gmail.com>
2025-04-29 17:55:00 -0700

    new files

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+let arr = []
+if (!argv[3]) {
+    console.log(0);
+} else {
+
+    let num = 2
+    for (let i = 2; i < argv.length; i++) {
+        arr.push(parseInt(argv[i]));
+    }
+//    console.log(arr.max);
+}
+
+
+function secondH () {
+
+    let max = -Infinity, sec=-Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] > max) {
+            sec1 = max;
+            max = arr[i];
+        } else if (arr[i] > sec && arr[i] !== max) {
+            sec = arr[i]
+        }
+    }
+    return (sec === -Infinity) ? 0 : sec;
+}
+
+console.log(secondH(arr));
